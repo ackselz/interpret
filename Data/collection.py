7a37a4ae988707645a930567c403a29a801027a4
ackselz <tanshaochong48@gmail.com>
2024-04-11 22:22:50 +0800

    add new data, new models and utility scripts

--- Data/collection.py
+++ Data/collection.py
@@ -3,24 +3,36 @@ import cv2
 import time
 import uuid
 
-number_of_classes = 1  #Number of words/phrases to train
+CATEGORY_LIST = [
+        # "CONNECT",
+        # "TIRED",
+        # "DETAIL",
+        # "HELP",
+        # "SEAT",
+        # -----
+        # "PLEASE",
+        # "TIRED",
+        "WIFI",
+        # "HOW",
+        # "SCHOOL",
+    ]  #Number of words/phrases to train
 images_per_class = 10  #Number of images per class
 esp32_url = 'http://192.168.12.24:81/stream'  #IP of the ESP32 camera
 
 RAW_IMAGES_DIR = 'RAW'
 
-for j in range(number_of_classes):
-    class_folder = os.path.join(RAW_IMAGES_DIR, f"Class_{j}")
+for category in CATEGORY_LIST:
+    class_folder = os.path.join(RAW_IMAGES_DIR, f"{category}")
     if not os.path.exists(class_folder):
         os.makedirs(class_folder)
 
     #Attempt to access the video stream
     cap = cv2.VideoCapture(esp32_url)
     if not cap.isOpened():
-        print(f"Error: Could not open video stream from class {j}.")
+        print(f"Error: Could not open video stream from class {category}.")
         continue
 
-    print(f'Starting data collection for class {j}')
+    print(f'Starting data collection for class {category}')
 
     number = 0
     start_time = time.time()
@@ -32,11 +44,11 @@ for j in range(number_of_classes):
             continue
         
         #Display the frame in a window
-        cv2.imshow(f"Class_{j} Frame", frame)
+        cv2.imshow(f"{category} frame", frame)
         cv2.waitKey(1)
 
         #Check if 5 seconds have passed since the last photo
-        if time.time() - start_time >= 5:
+        if time.time() - start_time >= 3:
             # Save the captured frame to disk
             image_name = f"{uuid.uuid4()}.jpg"
             cv2.imwrite(os.path.join(class_folder, image_name), frame)
--- Models/README.md
+++ Models/README.md
@@ -8,3 +8,13 @@
 20240409_gesture_recognizer.task Loki tuned
 20240409_gesture_recognizer_2.task Loki + SC default
 20240409_gesture_recognizer_3.task  Loki + SC tuned
+
+20240410_gesture_recognizer.task ['scissors', 'i', 'internet', 'please', 'rock', 'cannot', 'connect', 'where', 'whatsup', 'none', 'thankyou']
+20240410_gesture_recognizer_2.task ['scissors', 'i', 'internet', 'please', 'rock', 'cannot', 'connect', 'where', 'whatsup', 'none', 'thankyou'] tuned
+20240410_gesture_recognizer_3.task ['scissors', 'i', 'detail', 'please', 'rock', 'cannot', 'connect', 'where', 'whatsup', 'none', 'tired', 'thankyou']
+20240410_gesture_recognizer_4.task ['scissors', 'i', 'detail', 'please', 'rock', 'cannot', 'connect', 'where', 'whatsup', 'none', 'tired', 'thankyou'] tuned
+
+20240411_gesture_recognizer.task ['please', 'tired', 'connect', 'whatsup', 'cannot', 'none', 'thankyou', 'seat', 'help', 'where', 'rock', 'i']
+20240411_gesture_recognizer_2.task ['please', 'tired', 'connect', 'whatsup', 'cannot', 'none', 'thankyou', 'seat', 'help', 'where', 'rock', 'i'] tuned
+20240411_gesture_recognizer_3.task ['please', 'tired', 'connect', 'whatsup', 'cannot', 'wifi', 'none', 'thankyou', 'seat', 'help', 'where', 'rock', 'i']
+20240411_gesture_recognizer_4.task ['please', 'tired', 'connect', 'whatsup', 'cannot', 'wifi', 'none', 'thankyou', 'seat', 'help', 'where', 'rock', 'i'] tuned
