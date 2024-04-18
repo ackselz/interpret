32e20c8edd5b1dc41853d2cc6c635e7874697ddc
ackselz <tanshaochong48@gmail.com>
2024-04-18 13:37:08 +0800

    add web server and fly.io config

---
+++ Web/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const WebSocket = require('ws');
+const app = express();
+app.use(express.json({ extended: false }));
+app.use(express.static('public'));
+
+var port = process.env.PORT || 3000;
+console.log("Server started on port " + port)
+const server = new WebSocket.Server({ server: app.listen(port) });
+
+server.on('connection', (socket) => {
+  socket.on('ping', () => {
+    console.log('Received ping');
+    ws.pong();
+  });
+
+  socket.on('message', (msg) => {
+    server.clients.forEach(client => {
+      client.send(msg);
+    })
+  });
+});
